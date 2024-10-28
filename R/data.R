2fe3e3fea67ae1db040cdbaf992b17f7748591fb
Joshua Lambert <joshua.lambert@lshtm.ac.uk>
2024-10-28 12:55:38 +0000

    add data documentation

---
+++ R/data.R
@@ -0,0 +1,9 @@
+#' A list of epidemiological parameters.
+#'
+#' @format ## `epiparameterDB`
+#' A `list` of epidemiological parameters read from `extdata/parameters.json`
+#' with `r length(epiparameterDB::epiparameterDB)` database entries. The object
+#' is tagged with the class attribute `<epiparamterDB>` to validate the
+#' parameter library has been loaded using the `{epiparameterDB}` package.
+#'
+"epiparameterDB"

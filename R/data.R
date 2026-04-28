2d2e8693720d468acabe189789186a538733c424
Joshua Lambert <joshua.lambert@lshtm.ac.uk>
2026-04-28 14:50:16 +0100

    fix typo in data.R documentation

--- R/data.R
+++ R/data.R
@@ -3,7 +3,7 @@
 #' @format ## `epiparameterDB`
 #' A `list` of epidemiological parameters read from `extdata/parameters.json`
 #' with `r length(epiparameterDB::epiparameterDB)` database entries. The object
-#' is tagged with the class attribute `<epiparamterDB>` to validate the
+#' is tagged with the class attribute `<epiparameterDB>` to validate the
 #' parameter library has been loaded using the `{epiparameterDB}` package.
 #'
 "epiparameterDB"
